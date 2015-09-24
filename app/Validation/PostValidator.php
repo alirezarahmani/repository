e1233dbad43f746f74aff10d9eaef5836066f8e9
alireza <alirezarahmani@live.com>
2015-09-24 21:54:48 +0330

    first commit

---
+++ app/Validation/PostValidator.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Validation;
+
+use Validator;
+
+class PostValidator
+{
+    public static function validate($input)
+    {
+        $rules = [
+            'title' => 'Required|Min:4|Max:80|alpha_spaces',
+            'body' => 'Required',
+        ];
+        return Validator::make($input, $rules);
+
+    }
+}
